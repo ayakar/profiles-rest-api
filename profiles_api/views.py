c4619112138cd048945addfd0a31800d7c0866fc
Aya Y <ayay@Ayas-MacBook-Pro.local>
2021-03-01 22:06:13 -0500

    added test api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+# importing APIView object in views module of Django Rest Framework
+from rest_framework.views import APIView
+# importing Response object in response module of Django Rest Framework
+from rest_framework.response import Response 
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test API View"""
+
+#class always needs self as first parameter, fromat=None is not nessesary here but it's good practice
+    def get(self, request, format=None):
+        """Returns a list of APIView features"""
+        an_apiview=[
+            'my first string',
+            'second string',
+            'third string',
+            'forth string',
+        ]
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
